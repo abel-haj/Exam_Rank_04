a1967f487de826ae0254e502f0703f21535ba8c2
Abdelaziz <techproduct@hotmail.com>
2022-03-04 11:11:25 +0100

    basic simple command

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 
 
-
+#include <sys/wait.h>
 #include <unistd.h>
 
 void	ft_putstr_fd(int fd, char *s)
@@ -11,23 +11,36 @@ void	ft_putstr_fd(int fd, char *s)
 	}
 }
 
-int	main ( int ac, char **av, char *envp )
+int	main ( int ac, char **av, char **envp )
 {
 	int	ret;
 	pid_t	pid;
+	char	*path;
+	int	status;
 
+	path = av[1];
+	char *args[] = {path, av[2], NULL};
 	pid = fork();
 
-	// child process
-	if (pid == 0)
+	if (pid < 0)
 	{
-		ret = execve(envp);
+		ft_putstr_fd(2, "fatal: error\n");
+	}
+	// child process
+	else if (pid == 0)
+	{
+		ret = execve(path, args, envp);
 		if (ret == -1)
 		{
-			ft_putstr_fd(2, );
-			ft_putstr_fd(2, );
-			ft_putstr_fd(2, );
+			ft_putstr_fd(2, "error: cannot execute ");
+			ft_putstr_fd(2, path);
+			ft_putstr_fd(2, "\n");
 		}
+	}
+	// parent process
+	else
+	{
+		waitpid(pid, &status, WCONTINUED);
 	}
 
 	return 0;
